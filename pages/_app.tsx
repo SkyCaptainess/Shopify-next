cd8394c2511395d392ed69b7d320347ad5327022
SkyCaptainess <andychris9474@gmail.com>
2021-11-28 19:50:59 +0800

    Added nprogress top progressbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,22 @@
 import { ApolloProvider } from '@apollo/client';
 import type { AppProps } from 'next/app';
 import { DefaultSeo } from 'next-seo';
+import Router from 'next/router';
+import nProgress from 'nprogress';
 
 import Layout from '../components/common/Layout';
 import { CartProvider } from '../contexts/CartContext';
 import { client } from '../lib/apollo-client';
 import seo from '../config/seo.json';
 
-import '../styles/globals.css';
 import 'swiper/css';
 import 'swiper/css/navigation';
+import 'nprogress/nprogress.css';
+import '../styles/globals.css';
+
+Router.events.on('routeChangeStart', nProgress.start);
+Router.events.on('routeChangeError', nProgress.done);
+Router.events.on('routeChangeComplete', nProgress.done);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
